c0f827742fa9f50dfbb2f38fa79106cea6a63b31
ocaas <oliviaacas@gmail.com>
2025-01-16 05:04:26 +0100

    closes window

--- map.c
+++ map.c
@@ -6,13 +6,16 @@
 /*   By: olopez-s <olopez-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 01:19:55 by olopez-s          #+#    #+#             */
-/*   Updated: 2025/01/16 02:52:28 by olopez-s         ###   ########.fr       */
+/*   Updated: 2025/01/16 05:03:42 by olopez-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./mlx/mlx.h"
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <stdio.h>
+#include <X11/X.h>
+#include <X11/keysym.h>
 
 typedef struct s_map
 {
@@ -29,6 +32,56 @@ typedef struct s_data
 	t_map		*map;
 }	t_data;
 
+/*
+typedef struct s_data
+{
+	void *mlx_ptr;
+	void *win_ptr;
+} t_data;
+*/
+
+int on_destroy(t_data *data)
+{
+	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
+	exit(0);
+	return (0);
+}
+
+int on_keypress(int keysym, t_data *data)
+{
+	(void)data;
+    if (keysym == XK_Escape) // Escape key
+    {
+        on_destroy(data);
+    }
+    printf("Pressed key: %d\n", keysym);
+    return (0);
+}
+
+int main(void)
+{
+	t_data data;
+
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (1);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
+	if (!data.win_ptr)
+		return (free(data.mlx_ptr), 1);
+
+	// Register key release hook
+	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+
+	// Register destroy hook
+	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+
+	// Loop over the MLX pointer
+	mlx_loop(data.mlx_ptr);
+	return (0);
+}
+/*
 int main(void)
 {
 	void *mlx_ptr;
@@ -46,3 +99,4 @@ int main(void)
 	free(mlx_ptr);
 	return (0);
 }
+*/
